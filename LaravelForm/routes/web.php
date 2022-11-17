c55c6456c000d9a93d7b00baa83419df43be3437
gendhat <rockyrush1994@gmail.com>
2022-11-17 19:59:57 +0700

    update file

--- LaravelForm/routes/web.php
+++ LaravelForm/routes/web.php
@@ -16,3 +16,8 @@ Route::get('/', function () {
 });
 Route::get('add-blog-post-form', [PostController::class, 'index']);
 Route::post('store-form', [PostController::class, 'store']);
+
+Route::get('/read',[PostController::class,'read']);
+Route::get('/edit/{id}', [PostController::class, 'edit']);
+Route::get('/delete/{id}', [PostController::class, 'destroy']);
+Route::put('/update/{id}', [PostController::class, 'update']);
